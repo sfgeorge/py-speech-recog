6e93262f6173a7f1281620cda1e9d22ba2ea6608
Stephen George <sgit-turtle.testudo12@wronghead.com>
2021-06-25 11:54:27 -0400

    First attempt at recognition

--- speech_recog.py
+++ speech_recog.py
@@ -6,9 +6,19 @@ import sys
 def run(audio_path):
     errprint("Processing '" + audio_path + "'")
     errprint()
+    recognizer = speech_recognition.Recognizer()
+
+    transcription = process_audio_data(audio_data = audio_path, recognizer = recognizer)
+
+    return transcription
 
 def errprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+def process_audio_data(audio_data, recognizer):
+    # recognize speech using Sphinx
+    transcription = recognizer.recognize_sphinx(audio_data)
+    print(transcription)
+    return transcription
 
 transcription = run(sys.argv[1])
