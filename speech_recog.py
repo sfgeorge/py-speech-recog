6b2177c05aa29e4d905d1a63d7b6a98560036177
Stephen George <sgit-turtle.testudo12@wronghead.com>
2021-09-12 05:44:36 -0400

    Pass show_all as an argument

--- speech_recog.py
+++ speech_recog.py
@@ -11,17 +11,17 @@ def run(audio_path):
     with speech_recognition.AudioFile(audio_path) as source:
         audio_data = recognizer.record(source)  # read the entire audio file
 
-    transcription = process_audio_data(audio_data = audio_data, recognizer = recognizer)
+    transcription = process_audio_data(audio_data = audio_data, recognizer = recognizer, show_all = False)
 
     return transcription
 
 def errprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def process_audio_data(audio_data, recognizer):
+def process_audio_data(audio_data, recognizer, show_all):
     # recognize speech using Sphinx
     try:
-       transcription = recognizer.recognize_sphinx(audio_data, language = "en-US")
+       transcription = recognizer.recognize_sphinx(audio_data, language = "en-US", show_all = show_all)
        print(transcription)
        return transcription
     except speech_recognition.UnknownValueError:
